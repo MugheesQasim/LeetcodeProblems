53e54798ad5bf983ff1ef62b9375e7ca15506f36
Mughees Qasim <45170085+MugheesQasim@users.noreply.github.com>
2022-09-04 14:57:13 +0000

    Sync LeetCode submission - Maximum Subarray (csharp)

---
+++ Problems/problems/maximum_subarray/solution.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int largestSum = nums[0];
+        int sum = 0;
+        for(int i=0;i<nums.Length;i++)
+        {
+            sum += nums[i];
+            
+            if(sum>largestSum)
+                largestSum = sum;
+            if(sum<=0)
+                sum = 0;
+        }
+        return largestSum;
+    }
+}
