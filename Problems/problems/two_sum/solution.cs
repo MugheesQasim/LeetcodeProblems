7cff3176a2521af85a5423b7982406f837dcd952
Mughees Qasim <45170085+MugheesQasim@users.noreply.github.com>
2022-09-06 10:03:57 +0000

    Sync LeetCode submission - Two Sum (csharp)

---
+++ Problems/problems/two_sum/solution.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int []returningIndexArray = {0,0};
+        
+        for(int i=0;i<nums.Length;i++){
+            for(int j=i+1;j<nums.Length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    returningIndexArray[0] = i;
+                    returningIndexArray[1] = j;
+                    
+                    return returningIndexArray;
+                }
+            }
+        }
+         return returningIndexArray;
+    }
+}
